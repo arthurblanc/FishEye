2b5fb101af0c6ac59d7f6f22f6becf4f8666fd7c
Arthur BLANC <arthur.blanc@outlook.com>
2021-09-05 18:32:31 +0400

    add tablet & desktop CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -60,4 +60,19 @@ async function render() {
 	renderPhotographersList();
 }
 render();
+
+/* TO CONTTENT BTN*/
+const toContentBtn = document.getElementById("to-content-btn");
+//toContentBtn.classList.add("to-content-btn-show");
+
+window.addEventListener("scroll", checkScrollPosition);
+
+function checkScrollPosition() {
+	if (window.pageYOffset >= 20) {
+		toContentBtn.classList.add("to-content-btn-show");
+	} else {
+		toContentBtn.classList.remove("to-content-btn-show");
+	}
+}
+
 /* ↑ INDEX ↑ */
