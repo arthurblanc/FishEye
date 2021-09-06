53564b211779e8508d7c56738b8f3e5242a020ae
Arthur BLANC <arthur.blanc@outlook.com>
2021-09-06 18:21:43 +0400

    add form

--- assets/js/photographer-page.js
+++ assets/js/photographer-page.js
@@ -67,6 +67,7 @@ async function render() {
 	for (let item in photographer.tags) {
 		const photographerTags = newElement("li", photographerTagsContainerList, { class: "about-photographer-tags tag " }, "#" + photographer.tags[item]);
 	}
+	const callToActionModalPhotographerName = newValue("call-to-action-modal-photographer-name", {}, photographer.name);
 	const photographerPrice = newValue("about-photographer-price", {}, photographer.price + "€ / jour");
 	let allLikes = 0;
 	for (let i = 0; i < mediasOfThisPhotograph.length; i++) {
@@ -105,4 +106,74 @@ async function render() {
 	const photographerLikes = newValue("about-photographer-likes-count", {}, allLikes);
 }
 render();
+
+// Launch modal event
+const modalbg = document.querySelector(".call-to-action-modal");
+const modalBtn = document.querySelectorAll("#call-to-action-button");
+const modalCloseBtn = document.querySelectorAll(".close1");
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Launch modal form
+function launchModal() {
+	modalbg.style.display = "block";
+}
+
+// Issue #1 : close modal
+// Close modal on click on X
+modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal1));
+// Close modal on click on confirmation button
+//confirmationCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// Close modal with a display="none"
+function closeModal1() {
+	modalbg.style.display = "none";
+}
+
+const form = document.getElementById("form");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+const errorFirst = document.getElementById("error-first");
+const errorLast = document.getElementById("error-last");
+const errorEmail = document.getElementById("error-email");
+const errorMessage = document.getElementById("error-message");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	// Regex for form validation
+	const nameRegex = /^([A-Za-zÀ-ÖØ-öø-ÿ][A-Za-zÀ-ÖØ-öø-ÿ ,.'-]*){2}$/g;
+	const emailRegex = /(?=^.{5,255}$)^([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,})$/g;
+	const messageRegex = /^([A-Za-zÀ-ÖØ-öø-ÿ0-9][A-Za-zÀ-ÖØ-öø-ÿ0-9 ,.'-]*){20}$/g;
+	let formValidated = true; // Need to be true to submit form
+
+	// Function to check form inputs with regex and add errors messages if inputs are not validated
+	function validateInput(input, regex, error) {
+		const validated = input.value.match(regex);
+		// If input is not validated : formValidated="false"
+		if (!validated) {
+			formValidated = false;
+			error.style.display = "block";
+			input.style.border = "0.2rem solid #fe142f";
+		} else {
+			error.style.display = "none";
+			input.style.border = "0.05rem solid #ccc";
+		}
+	}
+
+	validateInput(firstName, nameRegex, errorFirst);
+	validateInput(lastName, nameRegex, errorLast);
+	validateInput(email, emailRegex, errorEmail);
+	validateInput(message, messageRegex, errorMessage);
+
+	// Issue #4 : confirmation message
+	// If form is validated, hide form and show confirmation message
+	if (formValidated) {
+		confirmation.style.display = "block";
+		form.style.display = "none";
+	}
+});
+
 /* ↑ INDEX ↑ */
